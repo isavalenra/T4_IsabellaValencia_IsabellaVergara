d812553b8425e2a25780f32c021633e6c67a93d5
isavergara <isabella.vergara@udea.edu.co>
2024-05-29 22:12:35 -0500

    Agregar método crear_tabla_paciente

--- modelo.py
+++ modelo.py
@@ -5,3 +5,16 @@ class Modelo:
         self.nombre_db = nombre_db
         self.conexion = sqlite3.connect(self.nombre_db)
         self.crear_tabla_paciente()
+
+    def crear_tabla_paciente(self):
+        cursor = self.conexion.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS Paciente (
+                nombre TEXT NOT NULL,
+                apellido TEXT NOT NULL,
+                edad INTEGER NOT NULL,
+                identificacion TEXT PRIMARY KEY
+            )
+        ''')
+        self.conexion.commit()
+        cursor.close()
